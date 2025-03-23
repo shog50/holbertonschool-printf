34e21f19b095ac12940493ead0ecd182d85305d7
shog50 <10709@holbertonstudents.com>
2025-03-23 22:45:38 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- test/main.c
+++ test/main.c
@@ -1,13 +1,29 @@
-#include <stddef.h>
-#include "../main.h"
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Entry point for testing _printf.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    _printf("Simple sentence: Hello, world!\n");
-    _printf("Character: [%c]\n", 'H');
-    _printf("String: [%s]\n", "I am a string!");
-    _printf("Percent: [%%]\n");
-    _printf("NULL string: [%s]\n", NULL);
+    int len1, len2;
+
+    /* Test character specifier */
+    len1 = _printf("Character: [%c]\n", 'H');
+    len2 = printf("Character: [%c]\n", 'H');
+    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
+
+    /* Test string specifier */
+    len1 = _printf("String: [%s]\n", "Hello, World!");
+    len2 = printf("String: [%s]\n", "Hello, World!");
+    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
+
+    /* Test percent specifier */
+    len1 = _printf("Percent: [%%]\n");
+    len2 = printf("Percent: [%%]\n");
+    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
+
     return (0);
 }
-

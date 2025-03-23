935d1415509d65a67d28078c976c1d5fa5bdac2e
shog50 <10709@holbertonstudents.com>
2025-03-23 19:04:49 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- test/main.c
+++ test/main.c
@@ -1,12 +1,8 @@
-#include "main.h"
+#include "../main.h"
 
 int main(void)
 {
-    _printf("Simple sentence: Hello, world!\n");
-    _printf("Character: %c\n", 'H');
-    _printf("String: %s\n", "Hello, world!");
-    _printf("Percent: %%\n");
-    _printf("NULL string: %s\n", (char *)0);
-
+    _printf("Hello, world!\n");
     return (0);
 }
+

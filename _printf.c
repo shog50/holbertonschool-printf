0636aa0996c306c9031d3db96650b8d6227a9a07
shog50 <10709@holbertonstudents.com>
2025-03-24 01:26:28 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,49 +1,51 @@
-#include <stdarg.h>
+#include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
 * _printf - Produces output according to a format
-* @format: The format string containing the directives
-*
-* Return: The number of characters printed (excluding null byte)
+* @format: Format string containing the directives
+* Return: Number of characters printed (excluding the null byte)
 */
 int _printf(const char *format, ...)
 {
 int count = 0;
 va_list args;
+int i; /* Declare variables outside the loop */
 
 if (!format)
 return (-1);
 
 va_start(args, format);
 
-for (int i = 0; format[i] != '\0'; i++)
+for (i = 0; format[i] != '\0'; i++) /* Use declared variable */
 {
 if (format[i] == '%')
 {
 i++;
-if (format[i] == 'c') /* Character */
+if (format[i] == 'c')
 {
 char c = va_arg(args, int);
 write(1, &c, 1);
 count++;
 }
-else if (format[i] == 's') /* String */
+else if (format[i] == 's')
 {
 char *str = va_arg(args, char *);
+int j; /* Declare variables outside the inner loop */
 if (!str)
 str = "(null)";
-for (int j = 0; str[j] != '\0'; j++, count++)
+for (j = 0; str[j] != '\0'; j++, count++)
 write(1, &str[j], 1);
 }
-else if (format[i] == '%') /* Percent sign */
+else if (format[i] == '%')
 {
-write(1, "%", 1);
+printf("%%");
 count++;
 }
 else
 {
-/* Handle unknown specifier */
 write(1, "%", 1);
 write(1, &format[i], 1);
 count += 2;
--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 int handle_char(va_list args);
 int handle_string(va_list args);
 int handle_percent(void);

e99875a29ba2ba143254800c633508d033bca9f8
shog50 <10709@holbertonstudents.com>
2025-03-24 01:34:54 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, World!\n");
+    return (0);
+}
+
--- test/main.c
+++ test/main.c
@@ -1,30 +1,12 @@
-#include <stdio.h>
 #include "../main.h"
 
-/**
- * main - Entry point for testing _printf.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int len1, len2;
-
-    /* Test character specifier */
-    len1 = _printf("Character: [%c]\n", 'H');
-    len2 = printf("Character: [%c]\n", 'H');
-    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
-
-    /* Test string specifier */
-    len1 = _printf("String: [%s]\n", "Hello, World!");
-    len2 = printf("String: [%s]\n", "Hello, World!");
-    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
-
-    /* Test percent specifier */
-    len1 = _printf("Percent: [%%]\n");
-    len2 = printf("Percent: [%%]\n");
-    printf("Lengths: _printf: %d, printf: %d\n", len1, len2);
+    _printf("Simple sentence: Hello, world!\n");
+    _printf("Character: %c\n", 'H');
+    _printf("String: %s\n", "Hello, world!");
+    _printf("Percent: %%\n");
+    _printf("NULL string: %s\n", (char *)0);
 
     return (0);
 }
-	
